72266fbf2cd00620ed1f7a49b28dd81759eebdc5
Suzan Hamza <suzan.hamza@gmail.com>
2022-01-05 22:41:02 +0200

    Stateless Functional Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <ListContacts contacts={contacts} />
       </div>
     )
   }
---
+++ src/ListContacts.js
@@ -0,0 +1,27 @@
+import React from "react";
+// Stateless Functional Components
+function ListContacts (props) {
+    return (
+        <ol className="contact-list">
+                {props.contacts.map((contact) => (
+                    <li key={contact.id} className="contact-list-item">
+                        <div
+                            className="contact-avatar"
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}
+                        ></div>
+                        <div className="contact-details">
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
+                        <button className="contact-remove">
+                            Remove
+                        </button>
+                    </li>
+                ))}
+            </ol>
+    );
+}
+
+export default ListContacts
